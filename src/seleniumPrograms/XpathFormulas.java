e68730e8099ada97ac23a57e9ef524417c8297fd
pdkjayani <ponraj8785@gmail.com>
2021-01-11 18:37:38 +0530

    changes to xpath details

---
+++ src/seleniumPrograms/XpathFormulas.java
@@ -0,0 +1,33 @@
+package seleniumPrograms;
+
+
+public class XpathFormulas {
+
+
+	public static void main(String[] args) {
+
+		/**[@attributeName='attributeValue']
+		tagName[(text()='forget account?')]
+
+		tagName[starts-with(text(),'forget')]
+		tagName[starts-with(@attributeName,'forget')]
+
+		tagName[contains(text(),'?')]
+		tagName[contains(@attributeName,'forget')]
+
+		tagName[@attributeName='attributevalue']/ancestor::tagName[@attributeName='attributevalue']
+		tagName[@attributeName='attributevalue']/descendant::tagName[@attributeName='attributevalue']
+
+		tagName[@attributeName='attributevalue']/parent::tagName[@attributeName='attributevalue']
+		tagName[@attributeName='attributevalue']/child::tagName[@attributeName='attributevalue']
+
+		tagName[@attributeName='attributevalue']/following::tagName
+		tagName[@attributeName='attributevalue']/following-siblings::tagName
+
+		tagName[@attributeName='attributevalue']/preceding::tagName
+		tagName[@attributeName='attributevalue']/preceding-siblings::tagName*/
+
+
+	}
+
+}
